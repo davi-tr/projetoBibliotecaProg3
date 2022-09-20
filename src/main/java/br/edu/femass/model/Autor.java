5a07adc5522a0f55634608557b24b0740964a50a
davi-tr <davitrajano473@gmail.com>
2022-09-19 23:33:57 -0300

    Primera classe iniciada com seus respectivos testes

---
+++ src/main/java/br/edu/femass/model/Autor.java
@@ -0,0 +1,53 @@
+package br.edu.femass.model;
+
+import java.util.Objects;
+
+public class Autor {
+    private String nome;
+    private String sobreNome;
+    private String nacionalidade;
+
+    public Autor(String nome, String sobreNome, String nacionalidade){
+        this.nome = nome;
+        this.sobreNome = sobreNome;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Autor autor = (Autor) o;
+
+        if (!Objects.equals(nome, autor.nome)) return false;
+        if (!Objects.equals(sobreNome, autor.sobreNome)) return false;
+        return Objects.equals(nacionalidade, autor.nacionalidade);
+    }
+
+    }
+
---
+++ src/test/java/br/edu/femass/model/AutorTest.java
@@ -0,0 +1,61 @@
+package br.edu.femass.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutorTest {
+
+    @Test
+    void getNome() {
+        Autor autor = new Autor("Davi","Trajano","Brazil");
+        String esperado = "Davi";
+        Assertions.assertEquals(esperado, autor.getNome());
+    }
+
+    @Test
+    void setNome() {
+        Autor autor = new Autor("Davi","Trajano","Brazil");
+        String esperado = "Luis";
+        autor.setNome("Luis");
+        Assertions.assertEquals(esperado, autor.getNome());
+    }
+
+    @Test
+    void getSobreNome() {
+        Autor autor = new Autor("Davi","Trajano","Brazil");
+        String esperado = "Trajano";
+        Assertions.assertEquals(esperado, autor.getSobreNome());
+    }
+
+    @Test
+    void setSobreNome() {
+        Autor autor = new Autor("Davi","Trajano","Brazil");
+        String esperado = "França";
+        autor.setSobreNome("França");
+        Assertions.assertEquals(esperado, autor.getSobreNome());
+    }
+
+    @Test
+    void getNacionalidade() {
+        Autor autor = new Autor("Davi","Trajano","Brazil");
+        String esperado = "Brazil";
+        Assertions.assertEquals(esperado, autor.getNacionalidade());
+    }
+
+    @Test
+    void setNacionalidade() {
+        Autor autor = new Autor("Davi","Trajano","Brazil");
+        String esperado = "China";
+        autor.setNacionalidade("China");
+        Assertions.assertEquals(esperado, autor.getNacionalidade());
+    }
+
+    @Test
+    void testEquals() {
+        Autor Davi = new Autor("Davi","Trajano","Brazil");
+        Autor Davi2 = new Autor("Davi","Trajano","Brazil");
+        Assertions.assertTrue(Davi.equals(Davi2));
+    }
+}
