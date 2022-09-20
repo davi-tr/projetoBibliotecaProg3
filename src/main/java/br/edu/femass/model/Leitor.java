9e21e1f73de15d13bcbf68d9f2e5227dd38c9d8a
davi-tr <davitrajano473@gmail.com>
2022-09-19 23:47:20 -0300

    Classe Leitor criada

---
+++ src/main/java/br/edu/femass/model/Leitor.java
@@ -0,0 +1,55 @@
+package br.edu.femass.model;
+
+public class Leitor {
+    private Long codigo;
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private Integer prazoMaximoDevolucao;
+
+    public Leitor(String nome, String endereco, String telefone){
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getPrazoMaximoDevolucao() {
+        return prazoMaximoDevolucao;
+    }
+
+    public void setPrazoMaximoDevolucao(Integer prazoMaximoDevolucao) {
+        this.prazoMaximoDevolucao = prazoMaximoDevolucao;
+    }
+}
