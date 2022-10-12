acaca1a8bb2e6ce65e82d107474bf52a4f183460
davi-tr <davitrajano473@gmail.com>
2022-10-12 11:36:14 -0300

    Telas de menu elaboradas, funcionalidade pendente

---
+++ src/main/java/br/edu/femass/GUI/GuiMenuAten.java
@@ -0,0 +1,33 @@
+package br.edu.femass.GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuiMenuAten {
+    private JPanel jPanel;
+    private JButton realizarEmprestimoButton;
+    private JButton devolverEmprestimoButton;
+    private JButton voltarButton;
+
+    public GuiMenuAten() {
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        devolverEmprestimoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        realizarEmprestimoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
---
+++ src/main/java/br/edu/femass/GUI/GuiMenuBibli.java
@@ -0,0 +1,40 @@
+package br.edu.femass.GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuiMenuBibli {
+    private JPanel jPanel;
+    private JButton btnLivro;
+    private JButton btnAutores;
+    private JButton btnExemplar;
+    private JButton btnVoltar;
+
+    public GuiMenuBibli() {
+        btnLivro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnAutores.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnExemplar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
