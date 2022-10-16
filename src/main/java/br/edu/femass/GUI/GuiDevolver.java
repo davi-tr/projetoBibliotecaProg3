27c1455b1726de869e5a0bb336f0c96db4f70bed
davi-tr <davitrajano473@gmail.com>
2022-10-15 21:10:48 -0300

    Gui Devolução iniciado

---
+++ src/main/java/br/edu/femass/GUI/GuiDevolver.java
@@ -0,0 +1,9 @@
+package br.edu.femass.GUI;
+
+import javax.swing.*;
+
+public class GuiDevolver {
+    private JPanel jPanel;
+    private JComboBox cboEmprestimos;
+    private JTextField txtData;
+}
--- src/main/java/br/edu/femass/GUI/GuiEmprestimo.java
+++ src/main/java/br/edu/femass/GUI/GuiEmprestimo.java
@@ -9,6 +9,8 @@ import br.edu.femass.model.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,19 +22,22 @@ public class GuiEmprestimo {
     private JList lstAlunos;
     private JList lstProfessores;
     private JComboBox cboExemplares;
+    private JComboBox cboSeletor;
+    private JLabel txtTitulo;
     private JComboBox cboLeitores;
+    private Object professor = "Professor";
+    private Object aluno = "Aluno";
     List<Leitor> leitor = new ArrayList();
 
     public GuiEmprestimo() {
         updateList();
         updateCombo();
+        valores();
         salvarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try{
-                    Emprestimos emprestimos = new Emprestimos(cboExemplares.getSelectedItem(),lstProfessores.getSelectedValuesList());
-                    updateCombo();
-                    updateList();
+                    Emprestimos emprestimos = new Emprestimos(cboExemplares.getSelectedItem(),lstAlunos.getSelectedValuesList());
                     try {
                         new DaoEmprestimo().save(emprestimos);
                     }catch (ParseException p){
@@ -41,6 +46,32 @@ public class GuiEmprestimo {
                 }catch (Exception ex){
                     //JOptionPane.showMessageDialog(null,ex.getMessage());
                     System.out.println(ex.getMessage());
+                }
+            }
+        });
+        cboSeletor.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(cboSeletor.getSelectedItem()== aluno){
+                    try {
+                        txtTitulo.setText("Aluno");
+                        List<Aluno> alunos = new DaoAluno().getAll();
+                        List<Professor> professores = new DaoProfessor().getAll();
+                        lstAlunos.setListData(alunos.toArray());
+
+                    } catch (Exception es) {
+                        throw new RuntimeException(es);
+                    }
+                } else{
+                    try {
+                        txtTitulo.setText("Professor");
+                        List<Aluno> alunos = new DaoAluno().getAll();
+                        List<Professor> professores = new DaoProfessor().getAll();
+                        lstAlunos.setListData(professores.toArray());
+
+                    } catch (Exception es) {
+                        throw new RuntimeException(es);
+                    }
                 }
             }
         });
@@ -58,13 +89,15 @@ public class GuiEmprestimo {
         }
     }
 
-
+private void valores(){
+        cboSeletor.addItem(aluno);
+        cboSeletor.addItem(professor);
+}
     private void updateList() {
         try {
             List<Aluno> alunos = new DaoAluno().getAll();
             List<Professor> professores = new DaoProfessor().getAll();
             lstAlunos.setListData(alunos.toArray());
-            lstProfessores.setListData(professores.toArray());
 
         } catch (Exception e) {
             throw new RuntimeException(e);
